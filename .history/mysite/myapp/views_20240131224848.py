c00704619c9b37d280288181aa5cf03ab1f349c8
bitmap357 <edakoyeram@gmail.com>
2024-02-01 22:34:14 +0000

    ~c

---
+++ .history/mysite/myapp/views_20240131224848.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+    return HttpResponse('<h1>Hello Word</h1>')
+
+
+def products(request):
+    return HttpResponse('Products')
