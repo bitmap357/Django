1721d2dea26e1ff36027e0a2ad29af2748b9df66
bitmap357 <edakoyeram@gmail.com>
2024-02-08 16:56:48 +0000

    Db edit

--- .history/mysite/myapp/models_20240208165009.py
+++ .history/mysite/myapp/models_20240208165009.py
@@ -3,9 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Book(models.Model):
-    
-    
     name = models.CharField(max_length=100)
     desc = models.CharField(max_length=300)
     price = models.IntegerField()
+    
     
--- .history/mysite/myapp/models_20240208165013.py
+++ .history/mysite/myapp/models_20240208165013.py
@@ -3,8 +3,6 @@ from django.db import models
 # Create your models here.
 
 class Book(models.Model):
-    
-    
     name = models.CharField(max_length=100)
     desc = models.CharField(max_length=300)
     price = models.IntegerField()
