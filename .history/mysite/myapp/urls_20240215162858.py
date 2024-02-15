518438d184884afc0d455a50698cec5d6ec1dcbc
bitmap357 <edakoyeram@gmail.com>
2024-02-15 16:29:59 +0000

    namespacing

--- .history/mysite/myapp/urls_20240215162858.py
+++ .history/mysite/myapp/urls_20240215162858.py
@@ -3,7 +3,7 @@ from django.urls import path
 from myapp import views
 
 
-app_name = 'myapp'
+app_
 urlpatterns = [
     path('index/', views.index, name='index'),
     path('book/<int:book_id>/', views.detail, name='detail'),
