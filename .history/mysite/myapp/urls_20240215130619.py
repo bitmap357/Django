6380e1e3654e468145d76201994e27bf9b219f4a
bitmap357 <edakoyeram@gmail.com>
2024-02-15 13:50:27 +0000

    separating urls

---
+++ .history/mysite/myapp/urls_20240215130619.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from myapp import views
+
+urlpatterns = [
+    path('index/', views.index, name='index'),
+    path('book/<int:book_id>/', views.detail, name='detail'),
+]
