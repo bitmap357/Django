476e7ed2e7489262e177db090c0a55ac463c25f4
bitmap357 <edakoyeram@gmail.com>
2024-02-16 23:15:21 +0000

    adding image upload

--- .history/mysite/mysite/urls_20240216231328.py
+++ .history/mysite/mysite/urls_20240216231328.py
@@ -22,4 +22,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myapp.urls')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.M_URL, document_root=settings.MEDIA_ROOT)
