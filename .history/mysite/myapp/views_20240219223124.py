066e66fce330ac9ef0d06552b20c41b0ad162d00
bitmap357 <edakoyeram@gmail.com>
2024-02-19 22:35:42 +0000

    submiting data

---
+++ .history/mysite/myapp/views_20240219223124.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Book
+
+# Create your views here.
+
+def index(request):
+    book_list = Book.objects.all()
+    context = {
+        'book_list':book_list
+    }
+    return render(request, 'myapp/index.html', context)
+
+
+def detail(request, book_id):
+    book = Book.objects.get(id=book_id)
+    
+    return render(request, 'myapp/detail.html', {'book':book})
+
+def add_book(request):
+    if request.method == 'POST':
+        name = request.POST.get('name',)
+        desc = request.POST.get('desc',)
+        price = request.POST.get('price',)
+        book_image = request.FILE['book_image']
+        
+        book = Book(name=name, desc=desc, price=price, book_image=book_image)
+        book.save()
+        
+    return render(request, 'myapp/add_book.html')
